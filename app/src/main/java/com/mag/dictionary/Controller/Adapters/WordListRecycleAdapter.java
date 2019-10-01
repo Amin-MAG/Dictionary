2d0386adfa9a1da86aa97ffced838e0f4f203601
Amin-MAG <ghasvari313@gmail.com>
2019-10-01 15:43:30 +0330

    simple search view

--- app/src/main/java/com/mag/dictionary/Controller/Adapters/WordListRecycleAdapter.java
+++ app/src/main/java/com/mag/dictionary/Controller/Adapters/WordListRecycleAdapter.java
@@ -1,4 +1,52 @@
 package com.mag.dictionary.Controller.Adapters;
 
-public class WordListRecycleAdapter {
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mag.dictionary.Controller.Fragments.WordListFragment;
+import com.mag.dictionary.R;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class WordListRecycleAdapter extends RecyclerView.Adapter<WordListRecycleAdapter.WordListRecyclerViewHolder> {
+
+    private Activity activity;
+    private HashMap<String,String> words;
+
+    public WordListRecycleAdapter(HashMap<String,String> words) {
+        this.words = words;
+    }
+
+    @NonNull
+    @Override
+    public WordListRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        activity = (Activity) parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(activity);
+        View view = inflater.inflate(R.layout.layout_word_item, parent, false);
+        return new WordListRecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordListRecyclerViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class WordListRecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        public WordListRecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
--- app/src/main/java/com/mag/dictionary/Model/Repository.java
+++ app/src/main/java/com/mag/dictionary/Model/Repository.java
@@ -3,6 +3,8 @@ package com.mag.dictionary.Model;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.HashMap;
+
 public class Repository {
 
     // Singleton
@@ -26,4 +28,20 @@ public class Repository {
 
     // Word Management
 
+    private HashMap<String, String> data;
+
+    public HashMap<String, String> getData() {
+        HashMap<String,String> data = new HashMap<>();
+
+        data.put("car", "ماشین");
+        data.put("wash", "شستن");
+        data.put("buy", "خریدن");
+        data.put("sell", "فروختن");
+        data.put("cry", "گریه کردن");
+        data.put("love", "دوست داشتن");
+        data.put("school", "مدرسه");
+        data.put("radio", "رادیو");
+
+        return data;
+    }
 }
