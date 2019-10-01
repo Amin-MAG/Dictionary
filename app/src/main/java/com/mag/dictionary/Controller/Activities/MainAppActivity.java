6fcfb6d8ea2c7d35037bc9753acc402a3a77a597
Amin-MAG <ghasvari313@gmail.com>
2019-10-02 02:24:12 +0330

    simple add word dialog

--- app/src/main/java/com/mag/dictionary/Controller/Activities/MainAppActivity.java
+++ app/src/main/java/com/mag/dictionary/Controller/Activities/MainAppActivity.java
@@ -9,11 +9,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.mag.dictionary.Controller.Fragments.AddWordFragment;
 import com.mag.dictionary.Controller.Fragments.MainAppFragment;
 import com.mag.dictionary.R;
 
 public class MainAppActivity extends SingleFragmentActivity {
+
+    private static final int REQUEST_CODE_FOR_ADD_WORD = 1001;
+    private static final String ADD_WORD_FRAGMENT = "add_word_fragment";
+
 
     @Override
     public Fragment getFragment() {
@@ -30,12 +36,10 @@ public class MainAppActivity extends SingleFragmentActivity {
         super.onCreate(savedInstanceState);
 
 
-
         this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         getSupportActionBar().setCustomView(R.layout.layout_app_custom_toolbar);
 //        ((TextView) getSupportActionBar().getCustomView().findViewById(R.id.userInfoFragment_username)).setText(Global.getOnlineUsername());
-
 
 
     }
@@ -51,6 +55,22 @@ public class MainAppActivity extends SingleFragmentActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mainMenu_add:
+
+                AddWordFragment addWordFragment = AddWordFragment.newInstance();
+                addWordFragment.setTargetFragment(getSupportFragmentManager().findFragmentByTag("tag_main_app_fragment"), REQUEST_CODE_FOR_ADD_WORD);
+
+                addWordFragment.show(getSupportFragmentManager(), ADD_WORD_FRAGMENT);
+                return true;
+            case R.id.mainMenu_exit:
+
+                finish();
+
+                return true;
+            default:
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
 
--- app/src/main/java/com/mag/dictionary/Controller/Fragments/AddWordFragment.java
+++ app/src/main/java/com/mag/dictionary/Controller/Fragments/AddWordFragment.java
@@ -1,8 +1,127 @@
 package com.mag.dictionary.Controller.Fragments;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.mag.dictionary.R;
 
 public class AddWordFragment extends DialogFragment {
 
-    
+
+    private TextInputEditText enWord, faWord;
+    private MaterialButton create, cancel;
+
+
+    public static AddWordFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        AddWordFragment fragment = new AddWordFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_word, container, false);
+    }
+
+    @SuppressLint("ResourceType")
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_add_word, null, false);
+
+        // Title
+
+        String titleText = getString(R.string.add_word);
+        ForegroundColorSpan foregroundColorSpanTtile = new ForegroundColorSpan(Color.parseColor(getResources().getString(R.color.white)));
+        SpannableStringBuilder titleString = new SpannableStringBuilder(titleText);
+        titleString.setSpan(
+                foregroundColorSpanTtile,
+                0,
+                titleText.length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+
+        // Dialog Box
+
+        Dialog dialog = new AlertDialog.Builder(getActivity())
+                .setTitle(titleString)
+                .setView(view)
+                .create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.parseColor(getResources().getString(R.color.colorPrimaryDark))));
+
+
+        findItems(view);
+
+        setEvents();
+
+        return dialog;
+    }
+
+    private void findItems(View view) {
+        faWord = view.findViewById(R.id.addWordFragment_faText);
+        enWord = view.findViewById(R.id.addWordFragment_enText);
+        create = view.findViewById(R.id.addTaskFragment_create);
+        cancel = view.findViewById(R.id.addTaskFragment_cancel);
+    }
+
+    private void setEvents() {
+
+        final Fragment fragment = getTargetFragment();
+        final Intent intent = new Intent();
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                String taskTitle = title.getText().toString();
+//                String taskDescription = description.getText().toString();
+//                Date taskDate = selectedDate;
+//
+//                try {
+//                    if (taskTitle.equals(Constants.EMPTY_STRING) || taskDescription.equals(Constants.EMPTY_STRING))
+//                        throw new EmptyFieldException();
+//                    Repository.getInstance(getContext()).addTaskForUser(Global.getOnlineUserID(),new Task(taskTitle, taskDescription, taskDate.getTime(), TaskStatus.TODO));
+//                } catch (EmptyFieldException e) {
+//                    intent.putExtra(DIALOG_ERROR,e.getMessage() );
+//                    intent.putExtra(HAS_ERROR,1 );
+//                } finally {
+//                    fragment.onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
+//                    dismiss();
+//                }
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragment.onActivityResult(getTargetRequestCode(), Activity.RESULT_CANCELED, intent);
+                dismiss();
+            }
+        });
+
+    }
+
+
 }
